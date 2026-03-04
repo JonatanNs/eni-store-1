ed9c06fe19b59a412993c39acdd3c9c26db13f90
JonatanNs <nsualu.jonatan@gmail.com>
2026-03-04 11:43:52 +0100

    feat : add stucture projet gradle

---
+++ src/test/java/fr/eni/tpenistore1/TpEniStore1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.tpenistore1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpEniStore1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
